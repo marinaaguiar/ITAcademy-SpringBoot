3fd67571243eb8768b0a0b15f867065a4e448c69
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-07-15 17:40:18 +0200

    complete n2exercise1

--- S04T01N02MarinaAlecrim/src/main/java/cat/itacademy/barcelonactiva/alecrim/marina/s04/t01/n02/Controllers/HelloWorldController.java
+++ S04T01N02MarinaAlecrim/src/main/java/cat/itacademy/barcelonactiva/alecrim/marina/s04/t01/n02/Controllers/HelloWorldController.java
@@ -1,2 +1,23 @@
-package cat.itacademy.barcelonactiva.alecrim.marina.s04.t01.n02.Controllers;public class HelloWorldController {
+package cat.itacademy.barcelonactiva.alecrim.marina.s04.t01.n02.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String greets(@RequestParam(defaultValue = "UNKNOWN") String name) {
+        return "Hello, " + name + ". You are running a Gradle project";
+    }
+
+    @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{name}"})
+    public String greets2(@PathVariable(required = false) String name) {
+        if (name == null) {
+            name = "UNKNOWN";
+        }
+        return "Hello, " + name + ". You are running a Gradle project";
+    }
 }
